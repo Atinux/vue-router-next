77d7816a34aa482fc807aa118605afd3425bbaba
Eduardo San Martin Morote <posva13@gmail.com>
2020-09-02 17:46:02 +0200

    feat(devtools): add devtools plugin

--- src/router.ts
+++ src/router.ts
@@ -979,7 +979,7 @@ export function createRouter(options: RouterOptions): Router {
       }
 
       if (__DEV__) {
-        addDevtools(app, router)
+        addDevtools(app, router, matcher)
       }
     },
   }
